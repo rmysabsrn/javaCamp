a0055b2add04b0efaae7a259b436d186a069bc9d
rum <rum@LAPTOP-4G7K17JT>
2021-05-23 19:58:27 +0300

    day6/3

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPositions.java
@@ -0,0 +1,36 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Entity
+@Table(name = "job_positions")
+@Data
+public class JobPositions {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+
+	
+	public JobPositions() {
+	}
+
+	public JobPositions(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+}
